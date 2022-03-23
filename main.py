b63840c02986a25eac0b34d2f152b868bef7b340
Michal <voj38077@spseol.cz>
2022-03-23 12:43:44 +0100

    skola + MIT

--- main.py
+++ main.py
@@ -16,7 +16,13 @@ class Application(tk.Tk):
     name = "Grafy"
 
     def __init__(self):
+
         super().__init__(className=self.name)
+
+        self.var_entryKonec = tk.IntVar()
+        self.var_entryStart = tk.IntVar()
+        self.var_entryF = tk.IntVar()
+        self.var_entryA = tk.IntVar()       
         self.title(self.name)
         self.bind("<Escape>", self.quit)
         self.lbl = tk.Label(self, text="Grafy")
@@ -25,19 +31,19 @@ class Application(tk.Tk):
         self.btnVypsat.pack()
         self.lbl1 = tk.Label(self, text="Počáteční čas")
         self.lbl1.pack()
-        self.entryStart  = tk.Entry(self)
+        self.entryStart  = tk.Entry(self, textvariable = self.var_entryStart)
         self.entryStart.pack()
         self.lbl2 = tk.Label(self, text="koncový čas")
         self.lbl2.pack()
-        self.entryKonec  = tk.Entry(self)
+        self.entryKonec  = tk.Entry(self, textvariable = self.var_entryKonec)
         self.entryKonec.pack()
         self.lbl3 = tk.Label(self, text="Frekvence")
         self.lbl3.pack()
-        self.entryF  = tk.Entry(self)
+        self.entryF  = tk.Entry(self, textvariable = self.var_entryF)
         self.entryF.pack()
         self.lbl4 = tk.Label(self, text="Amplituda")
         self.lbl4.pack()
-        self.entryA  = tk.Entry(self)
+        self.entryA  = tk.Entry(self, textvariable = self.var_entryA)
         self.entryA.pack()
         self.btnGraf = tk.Button(self, text="Načíst graf", command=self.graf)
         self.btnGraf.pack()
@@ -53,17 +59,13 @@ class Application(tk.Tk):
         super().quit()
     
     def graf(self):
-        self.f = self.entryF.get()
-        self.a = self.entryA.get()
-        self.k = self.entryKonec.get()
-        self.s = self.entryStart.get()
-        print(self.f)
-        print(self.a)
-        print(self.s)
-        print(self.k)
-
-        t=linspace(0,50e-3,300)
- 
+        self.f = self.var_entryF.get()
+        self.a = self.var_entryA.get()
+        self.k = self.var_entryKonec.get()
+        self.s = self.var_entryStart.get()
+        t = pl.linspace(self.s, 10/self.f, self.f*10000)
+        y = self.a * (pl.cos(2*pi*self.f*t ))
+        pl.plot(t,y)
 
         pl.title("výkon")
         pl.xlabel("t[s]")
